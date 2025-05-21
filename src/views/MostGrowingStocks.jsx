5094e729142db3d29018bbeed76f4bbc977e749b
Alexey Zauzin <lehadnk@gmail.com>
2025-05-21 23:33:44 +0300

    Reduced prediction period

--- src/views/MostGrowingStocks.jsx
+++ src/views/MostGrowingStocks.jsx
@@ -13,7 +13,7 @@ export default function MostGrowingStocks() {
             const currentDate = new Date("2018-31-01");
 
             // Мокаем дату прогноза как 2019-07-01
-            const forecastDate = new Date("2019-03-01");
+            const forecastDate = new Date("2019-01-31");
 
             const formattedCurrentDate = currentDate.toISOString().split('T')[0];
             const formattedForecastDate = forecastDate.toISOString().split('T')[0];
